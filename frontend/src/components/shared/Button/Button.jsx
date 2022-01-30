089ef78cabfc6d656dd465667f2ef8098b77ed6a
aayushb04 <74265400+aayushb04@users.noreply.github.com>
2022-01-30 18:26:48 +0530

    authentication commit

--- frontend/src/components/shared/Button/Button.jsx
+++ frontend/src/components/shared/Button/Button.jsx
@@ -5,8 +5,8 @@ import styles from './Button.module.css';
 const Button = ({text,onClick}) => {
     return (
         <button onClick={ onClick} className={styles.button}>
-                <span>{ text}</span>
-            <img classsName={ styles.arrow} src="/images/arrow-forward.png" alt="arrow" />
+                <span>{text}</span>
+            <img className={styles.arrow}  src="/images/arrow-forward.png" alt="arrow" />
         </button>
   );
 };
---
+++ frontend/src/pages/Activate/Activate.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const activate = () => {
+    return <div>
+      Activate comp.
+  </div>;
+};
+
+export default activate;
